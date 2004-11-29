4480f08242109823522f8e284919551836a8e8f6
plasma <plasma>
2004-11-29 20:00:56 +0000

    *** empty log message ***

---
+++ acme/link.php
@@ -0,0 +1,52 @@
+<?php
+include("cms/config.inc");
+include($conf["cmsbase"] . "/debuglib.inc");
+include($conf["cmsbase"] . "/ets.inc");
+include($conf["cmsbase"] . "/fkt_createsel.inc");
+include($conf["cmsbase"] . "/fkt_groups.inc");
+include($conf["cmsbase"] . "/fkt_db.inc");
+include($conf["cmsbase"] . "/fkt_etc.inc");
+include($conf["cmsbase"] . "/fkt_rte.inc");
+include($conf["cmsbase"] . "/fkt_date." . $conf["lang"] . ".inc");
+dbconnect();
+include($conf["admbase"] . "/lang/admin." . $conf["lang"] . ".inc");
+include($conf["admbase"] . "/auth.inc");
+?>
+<html>
+<head>
+<title>
+<?php $lang["admin_redaktionsbereich"] ?>
+</title>
+<link rel="stylesheet" type="text/css" href="acme.css">
+<style type="text/css">
+        @import "all.css"; /* just some basic formatting, no layout stuff */
+        body  {
+               margin:10px 10px 0px 10px;
+               padding:0px;
+              }
+</style>
+<script language="JavaScript" type="text/javascript" src="richtext.js"></script>
+</head>
+<body>
+<?php
+echo "<form action=\"".$conf["admurl"]."/link.php\" ENCTYPE=\"multipart/form-data\" method=\"post\">";
+echo "<table>\n";
+echo "<tr><td>Modul</td><td>\n";
+echo "<select size = \"1\" name=\"modul\">\n";
+$statement = "SELECT m.modul FROM acme_module m  where m.typ='C' order by m.modul";
+$result = dbquery($statement);
+if ( mysql_num_rows($result) > 0 ) {
+   while ($row = mysql_fetch_object($result)) {
+       if (checkgroupaccess($conf["user_groups"], $row->modul) == 1) {
+          echo "<option value=\"". $row->modul . "\" >" . $lang["admin_$row->modul"] . "</option>\n";       	  
+          }
+   }
+}
+echo "</select><input type=\"submit\" name = ok value=\"ok\">\n</td></tr></table>\n</form>\n";
+
+if (isset($modul)){
+	include($conf["admbase"] . "/link.".$modul.".inc");
+}
+?>
+</body>
+</html>
